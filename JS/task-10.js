273592f1cc7307e1d83bc4d075ec00d5886c9969
Mykola Kyrylenko <bulletbest@gmail.com>
2020-09-24 00:17:16 +0300

    last fix

--- JS/task-10.js
+++ JS/task-10.js
@@ -1,27 +1,12 @@
 import users from "./users.js";
 // Получить массив всех умений всех пользователей(поле skills), при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
 const getSortedUniqueSkills = (users) => {
-  // твой код
-  return users
-    .reduce(function (skills, user) {
-      user.skills.forEach(function (element) {
-        if (!skills.includes(element)) {
-          skills.push(element);
-        }
-      });
-
-      return skills;
-    }, [])
+  const usersSkills = users.reduce((usersSkills, user) => {
+    return usersSkills.concat(user.skills);
+  }, []);
+  return usersSkills
+    .filter((skill, i) => usersSkills.indexOf(skill) === i)
     .sort();
-
-  // return new Set(
-  //   users
-  //     .reduce((allSkills, user) => {
-  //       allSkills.push(...user.skills);
-  //       return allSkills;
-  //     }, [])
-  //     .sort()
-  // );
 };
 
 console.log(getSortedUniqueSkills(users));
